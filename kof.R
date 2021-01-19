1980e5f698bf11af36a95974452c5a2257cf2cd4
tosorakim <59444956+tosorakim@users.noreply.github.com>
2021-01-20 03:05:27 +0900

    updated

---
+++ kof.R
@@ -0,0 +1,22 @@
+setwd("/Users/macbook/Documents/itwill/R")
+kof <- read.csv("KOFEXM.csv")
+result <- aggregate(Channel~Event_Name,kof,length)
+names(result) <- c("Event_Name", "Event_Name ¼ö")
+result
+
+=="install"
+
+Channel_Name
+install
+af_complete_registration
+af_level5_achieved
+af_level8_achieved
+af_purchase
+D1
+NRU
+Lv5
+purchase
+rate
+D1
+RR
+Note

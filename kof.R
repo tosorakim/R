cf5ee6bcd8240a012f0705aa02b6d808ce210329
tosorakim <59444956+tosorakim@users.noreply.github.com>
2021-01-20 03:06:18 +0900

    edited

--- kof.R
+++ kof.R
@@ -3,20 +3,3 @@ kof <- read.csv("KOFEXM.csv")
 result <- aggregate(Channel~Event_Name,kof,length)
 names(result) <- c("Event_Name", "Event_Name ¼ö")
 result
-
-=="install"
-
-Channel_Name
-install
-af_complete_registration
-af_level5_achieved
-af_level8_achieved
-af_purchase
-D1
-NRU
-Lv5
-purchase
-rate
-D1
-RR
-Note
